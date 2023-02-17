c5c449332397dbf5387bcf935c65e85a0d41a347
GekeBwana <dbwana28@gmail.com>
2023-02-16 18:50:57 -0800

    Print If

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - This is a fundamental function
+ *
+ * Return: 0 Success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	Return(0);
+}
