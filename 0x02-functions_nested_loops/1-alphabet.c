abe318895c1f12b43f72806197eb9c61b99e1b90
GekeBwana <dbwana28@gmail.com>
2023-02-21 18:45:09 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main- print alphabet in lowercase
+ */
+
+void print_alphabet(void);
+{
+	char letter;
+
+	for (letter = 'a' ; letter = 'z' ; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
