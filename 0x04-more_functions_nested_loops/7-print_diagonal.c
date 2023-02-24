acc7831065b0b394429e60005c9fd4061d5af26c
GekeBwana <dbwana28@gmail.com>
2023-02-23 19:01:56 -0800

    Task 6

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,14 +6,15 @@
  */
 void print_diagonal(int n)
 {
-	if(n <=0)
+	if (n <= 0)
 	{
 		_putchar('\n');
-	}else
+	}
+	else
 	{
 		int i, j;
 
-		for (i = 0,i < n, i++)
+		for (i = 0, i < n, i++)
 		{
 			if (j == i)
 				putchar('\\');
